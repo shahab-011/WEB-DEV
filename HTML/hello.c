2064c7a4582524b928c9e10778cb7f0ce29e3f3d
shahab-011 <shahabalam2512@gmail.com>
2024-03-02 14:45:51 +0530

    Initial commit

---
+++ HTML/hello.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    
+ int a,b,c,e;
+ float d;
+ printf("Enter the value of a and b:\n");
+ scanf("%d%d",&a,&b);
+
+ c=a*b;
+ d=a/b;
+ e=a+b;
+ 
+ 
+ printf("Multiplication of a and is %d\n, Division of a and b is %d, addition of a and b is %d ",c,d,e);
+
+  
+
+    
+}
